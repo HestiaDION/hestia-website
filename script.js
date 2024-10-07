b4e267cbba4dad2c0037c20f91315b5119713786
Mateus MCA <mateus.araujo@germinare.org.br>
2024-10-07 07:39:49 -0300

    login page

--- script.js
+++ script.js
@@ -1,62 +1,76 @@
 let centerIndex = 2;
-const previous = document.getElementById('previous')
-const advantagesParent = document.querySelectorAll('.advantages')
-const next = document.getElementById('next')
-const loader = document.getElementById('preloader')
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
+const loader = document.getElementById("preloader");
+const closeForm = document.getElementById("close-form");
+const formContainer = document.getElementById("form-container");
+const signUp = document.getElementById("sign-up");
 
+previous.addEventListener("click", () => {
+  const advantagesElements = Array.from(
+    document.querySelectorAll(".advantage")
+  );
+  let item = advantagesElements.pop();
+  advantagesElements[0].parentNode.prepend(item);
+  advantagesElements.unshift(item);
 
-previous.addEventListener('click', () => {
-    const advantagesElements = Array.from(document.querySelectorAll('.advantage'))
-    let item = advantagesElements.pop()
-    advantagesElements[0].parentNode.prepend(item)
-    advantagesElements.unshift(item)
-    
-    advantagesElements.forEach((adventage, index) => {
-        position = Math.abs(index - centerIndex)
-    
-        if (position === 0){
-            adventage.classList.remove('submain')
-            adventage.classList.remove('hide')
-            adventage.classList.add('main')
-        } else if (position === 1){
-            adventage.classList.remove('main')
-            adventage.classList.remove('hide')
-            adventage.classList.add('submain')
-        } else {
-            adventage.classList.remove('main')
-            adventage.classList.remove('submain')
-            adventage.classList.add('hide')
-        }
-    });
-})  
+  advantagesElements.forEach((adventage, index) => {
+    position = Math.abs(index - centerIndex);
 
-next.addEventListener('click', () => {
-    const advantagesElements = Array.from(document.querySelectorAll('.advantage'))
-    let item = advantagesElements.shift()
-    advantagesElements[0].parentNode.appendChild(item)
-    advantagesElements.push(item)
-    
-    advantagesElements.forEach((adventage, index) => {
-        position = Math.abs(index - centerIndex)
-    
-        if (position === 0){
-            adventage.classList.remove('submain')
-            adventage.classList.remove('hide')
-            adventage.classList.add('main')
-        } else if (position === 1){
-            adventage.classList.remove('main')
-            adventage.classList.remove('hide')
-            adventage.classList.add('submain')
-        } else {
-            adventage.classList.remove('main')
-            adventage.classList.remove('submain')
-            adventage.classList.add('hide')
-        }
-    });
-})
+    if (position === 0) {
+      adventage.classList.remove("secondary");
+      adventage.classList.remove("hidden");
+      adventage.classList.add("main");
+    } else if (position === 1) {
+      adventage.classList.remove("main");
+      adventage.classList.remove("hidden");
+      adventage.classList.add("secondary");
+    } else {
+      adventage.classList.remove("main");
+      adventage.classList.remove("secondary");
+      adventage.classList.add("hidden");
+    }
+  });
+});
 
+next.addEventListener("click", () => {
+  const advantagesElements = Array.from(
+    document.querySelectorAll(".advantage")
+  );
+  let item = advantagesElements.shift();
+  advantagesElements[0].parentNode.appendChild(item);
+  advantagesElements.push(item);
 
-window.addEventListener("load", function(){
-    loader.classList.add('loaded')
-    document.body.classList.add('scrolling-allowed')
-})
+  advantagesElements.forEach((adventage, index) => {
+    position = Math.abs(index - centerIndex);
+
+    if (position === 0) {
+      adventage.classList.remove("secondary");
+      adventage.classList.remove("hidden");
+      adventage.classList.add("main");
+    } else if (position === 1) {
+      adventage.classList.remove("main");
+      adventage.classList.remove("hidden");
+      adventage.classList.add("secondary");
+    } else {
+      adventage.classList.remove("main");
+      adventage.classList.remove("secondary");
+      adventage.classList.add("hidden");
+    }
+  });
+});
+
+signUp.addEventListener("click", () => {
+  formContainer.classList.remove("closed-form");
+  document.body.classList.remove("scrolling-allowed");
+});
+
+closeForm.addEventListener("click", () => {
+  formContainer.classList.add("closed-form");
+  document.body.classList.add("scrolling-allowed");
+});
+
+window.addEventListener("load", function () {
+  loader.classList.add("loaded");
+  document.body.classList.add("scrolling-allowed");
+});
